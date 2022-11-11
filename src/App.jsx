c07717c0ee3302cd191bfc88fbc86764dd9b564d
unknown <ranajaypratap27@gmail.com>
2022-11-12 00:02:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="App">
+    {/* Header */}
+    <Header/>
+    
+    
+    
+    {/* TinderCards */}
+    {/* SwipeButton */}
+    </div>
+  );
+}
+
+export default App;
